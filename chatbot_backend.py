a93db98e0c65b6c39f44596e7f39161a7534da47
sumitpriye <sumitsinha125@gmail.com>
2026-09-07 20:11:18 +0530

    added streaming

--- chatbot_backend.py
+++ chatbot_backend.py
@@ -1,6 +1,6 @@
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
-from langchain_core.messages import BaseMessage, HumanMessage
+from langchain_core.messages import BaseMessage
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from langgraph.graph.message import add_messages
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
